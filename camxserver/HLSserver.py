8390d61e564066a1ded7a6a5f8cb13e34b43ea8d
Emil R. Lee <emil.raylee@gmail.com>
2021-09-04 14:16:06 -0400

    Cloud Beta

--- camxserver/HLSserver.py
+++ camxserver/HLSserver.py
@@ -14,19 +14,22 @@ client = MongoClient(port=27017)
 db = client.camx
 collection = db.users
 #change_stream = collection.watch()
-external_devices = []
-commands = []
-commands_ran = []
-builder = "ffmpeg -i"  
-options = "-f hls -hls_time 3 -hls_playlist_type event -hls_list_size 10 -hls_wrap 10 -live_start_index 9 -hls_segment_type fmp4 -filter:v fps=10"     
 
-@retry(backoff=3)
+@retry()
 def createRTSP(command):
         
     os.popen(command)
         
 
 def main():
+
+    external_devices = []
+    commands = []
+    commands_ran = []
+    builder = "ffmpeg -i"  
+    options = "-f hls -hls_time 10 -hls_playlist_type event -live_start_index -1 -hls_segment_type fmp4 -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 300 -filter:v fps=10"     
+
+
     i = 1
     while True:
         try:
@@ -46,11 +49,14 @@ def main():
                                 if keys == "uri":
                                     rtsp = values
                             if "{} {} {} {}{}/{}.m3u8".format(builder,rtsp,options,saveTo,hwId,hwId) not in commands:
-                                commands.append("{} {} {} {}{}/{}.m3u8".format(builder,rtsp,options,saveTo,hwId,hwId))                   
+                                
+                                moreOptions = options + " -strftime 1 -hls_segment_filename {}{}/%Y%m%d_%H-%M-%S.mp4".format(saveTo,hwId)
+                                commands.append("{} {} {} {}{}/{}.m3u8".format(builder,rtsp,moreOptions,saveTo,hwId,hwId))                   
             for command in commands:
                 if command not in commands_ran:
                     print("rtsp daemon created")
                     print(command)
+                    print("==============================")
                     commands_ran.append(command)
                     #subprocess.Popen(command, shell=False)
                     p = Process(target=createRTSP, args=(command,))
